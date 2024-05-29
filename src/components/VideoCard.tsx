36fc45722c2219cd5318c82cfbfd8fe116e356f3
Yushank <yushank2001@gmail.com>
2024-05-29 20:00:17 +0530

    initial commit - created VideoCard component

---
+++ src/components/VideoCard.tsx
@@ -0,0 +1,25 @@
+
+export function VideoCard(props: any) {
+    return <div>
+        <img src={props.image}></img>
+
+        <div className="grid grid-cols-12 pt-2">
+            <div className="col-span-1">
+                <img className={"rounded-full w-20 h-20"} src={props.thumbImage}></img>
+            </div>
+
+            <div className="col-span-11 pl-5">
+                <div>
+                    {props.title}
+                </div>
+                <div className="col-span-11 text-gray-600 text-base">
+                    {props.ChannelName}
+                </div>
+                <div className="col-span-11 text-gray-600 text-base">
+                    {props.views} | {props.uploadTime}
+                </div>
+            </div>
+
+        </div>
+    </div>
+}
